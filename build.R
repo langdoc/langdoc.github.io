b03d194fb37e960e7e181cbb5e97e75d9ab0416a
Niko Partanen <nikotapiopartanen@gmail.com>
2017-03-22 09:47:39 +0100

    started to customize this

--- build.R
+++ build.R
@@ -1,2 +1,8 @@
+library(tidyverse)
+
+dir(".", "^\\d.*.Rmd") %>% purrr::map(readr::read_lines) %>% 
+        unlist %>% stringr::str_extract("(?<=\\[@)([a-zEA_\\d-]+)") %>% 
+        purrr::discard(is.na(.) == T)
+rmarkdown::clean_site()
 rmarkdown::render_site()
 # browseURL("index.html")
