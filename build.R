807b226574d067f8c95316cab0e931a0c1ac3042
Niko Partanen <nikotapiopartanen@gmail.com>
2017-04-09 13:19:54 +0200

    adding changed files

--- build.R
+++ build.R
@@ -1,8 +1,15 @@
-library(tidyverse)
+# library(tidyverse)
+# library(stringr)
 
-dir(".", "^\\d.*.Rmd") %>% purrr::map(readr::read_lines) %>% 
-        unlist %>% stringr::str_extract("(?<=\\[@)([a-zEA_\\d-]+)") %>% 
-        purrr::discard(is.na(.) == T)
+# citations <- dir(".", "^\\d.*.(R|r)md") %>% purrr::map(readr::read_lines) %>%
+#         unlist %>% stringr::str_extract("(?<=\\[@)([a-zEA_\\d-]+)") %>%
+#         purrr::discard(is.na(.) == T) %>%
+#         purrr::discard(str_detect(., "^kpv_")) %>% 
+#         sort(.) %>% paste0('key : "', ., '"', collapse = " or ")
+
+# system(paste('bib2bib -ob bibliography.bib -c \'', citations, '\' ~/Dropbox/FRibliography/bibtex/FRibliography.bib'))
+# system(paste('bib2bib -ob bibliography.bib -c', "\"call-number : 'NP'\"", '~/Dropbox/FRibliography/bibtex/FRibliography.bib'))
+
 rmarkdown::clean_site()
 rmarkdown::render_site()
 # browseURL("index.html")
